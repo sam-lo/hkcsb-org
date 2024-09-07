ebf1760a7fcd1d8c7701720e8ec54c8881213672
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-09-08 00:07:04 +0800

    Add Responsive design to Landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,39 @@
 import React from "react";
 import concert from "@/public/concert.jpg";
 import Image from "next/image";
+import {ArrowRightIcon} from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
     <>
-      <div className="w-fit relative">
-        <Image src={concert} alt="Concert Hall" className="brightness-[0.5] -translate-y-5"/>
-        <div className="flex flex-col w-3/5 absolute inset-y-36 left-8">
-          <div className="w-full px-10 h-2 my-5 bg-red-800"/>
-          <div className="flex flex-col space-y-6">
-            <div className="font-serif font-bold text-left text-5xl w-fit">
+      <div className="w-full lg:h-[50rem] h-[40rem] overflow-hidden relative overflow-x-auto">
+        <Image src={concert} alt="Concert Hall" className="brightness-[0.3] object-cover h-full"/>
+        <div className="flex flex-col w-full lg:w-[50rem] absolute lg:inset-y-14 inset-0 left-0 sm:px-16 px-8 py-20">
+          <hr className="w-full px-10 border-4 my-5 border-red-800 brightness-125"/>
+          <div className="flex flex-col sm:space-y-6 space-y-3">
+            <div className="font-serif font-bold text-left md:text-5xl text-4xl w-fit">
               Carpe Symphonic Band
             </div>
-            <div className="text-left text-2xl opacity-80 w-fit">
+            <div className="text-left sm:text-2xl text-lg opacity-80 w-fit">
               香港卡爾佩管樂團
             </div>
           </div>
-          <div className="w-full px-10 h-1 my-5 bg-red-800"/>
-          <div className="flex flex-col space-y-4 text-lg opacity-80">
+          <hr className="w-full px-10 border-2 my-5 border-red-800 brightness-125"/>
+          <div className="flex flex-col space-y-4 sm:text-lg text-md opacity-80">
             <div>
               “Carpe”一詞源自拉丁文的“Carpe
               Diem”，直譯為“抓住今天”。這個短語提醒我們要活在當下，充分利用眼前的每一刻。然而，它的意義不僅僅是享受當下，更是強調在如今的每一次機會中，為未來的成長奠定基礎。
             </div>
             <div>
               我們希望聚集一群對音樂有深厚造詣與追求的樂手，特別是那些在學期間因環境限制而未能充分發揮潛力的學生。在這個團體中，我們追求卓越，並珍惜每一次演奏的機會，從中獲得成就感和滿足感。這不僅是一次音樂的旅程，更是自我成長和相互激勵的過程。我們相信，每一次的演出都是一次寶貴的經驗，每一次的練習都是對未來的投資。
+            </div>
+          </div>
+          <div className="hidden lg:block group bg-slate-50/80 hover:bg-slate-50 w-64 py-4 px-5 rounded-3xl my-8 transition-all duration-300">
+            <div className="flex items-center justify-between">
+              <div className="text-slate-700 text-xl">
+                即將舉行的音樂會
+              </div>
+              <ArrowRightIcon className="w-8 text-slate-700 group-hover:-rotate-45 transition-all "/>
             </div>
           </div>
         </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,21 @@
 import {menusItems} from "@/app/data/menuItems";
-import {HomeIcon} from "@heroicons/react/24/outline";
+import {Bars3Icon, HomeIcon} from "@heroicons/react/24/outline";
 
 export default function Header() {
   const filteredItems = menusItems.filter(item => item.id >= 1);
   return (
     <>
-      <div className="absolute z-10 inset-x-0 bg-slate-700 rounded-b-3xl shadow-2xl shadow-black/80">
-        <div className="flex items-center justify-start space-x-8 py-5 px-8">
+      <div className="absolute z-10 inset-x-0 lg:bg-slate-700 rounded-b-3xl lg:shadow-2xl shadow-black/80">
+        <div className="items-center justify-start space-x-8 py-5 px-8 hidden lg:flex">
           <HomeIcon className="w-12 p-2 rounded-3xl text-slate-50 hover:text-slate-700 hover:bg-slate-50 transition-all duration-300"/>
           {filteredItems.map((item) => (
             <a href={item.href} key={item.id} className="py-3 px-5 rounded-3xl text-xl text-center text-slate-50 hover:text-slate-700 hover:bg-slate-50 transition-all duration-300">
               {item.name}
             </a>
           ))}
+        </div>
+        <div className="w-full flex lg:hidden items-center justify-end py-8 sm:px-14 px-8">
+          <Bars3Icon className="w-12" />
         </div>
       </div>
     </>
