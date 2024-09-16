1286f248a4e5c1f45e302c4bb683f06d6b03cdf6
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-09-16 22:17:55 +0800

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from "next";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -19,6 +20,7 @@ export default function RootLayout({children}: LayoutProps) {
     <body>
     <Header/>
     {children}
+    <SpeedInsights/>
     <Footer/>
     </body>
     </html>
