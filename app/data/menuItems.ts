3eab35557ee9c68d96b0158eae369a637636f438
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-09-29 01:38:20 +0800

    change wording again

--- app/data/menuItems.ts
+++ app/data/menuItems.ts
@@ -27,7 +27,7 @@ export const menusItems = [
     id: 2,
     name: "音樂會與門票",
     description: "查看音樂會資訊與購票",
-    fullDescription: "我們每年都會舉辦多場音樂會，請點擊這裡查看音樂會的資訊與購票方法。",
+    fullDescription: "樂團每年都會舉辦多場音樂會，請點擊這裡查看音樂會的資訊與購票方法。",
     href: "/concert",
     icon: TicketIcon,
   },
@@ -35,7 +35,7 @@ export const menusItems = [
     id: 3,
     name: "加入樂團",
     description: "加入Carpe的大家庭",
-    fullDescription: "如果你對音樂有熱情，歡迎參加我們的試音，我們歡迎各種樂器的樂手加入！",
+    fullDescription: "如果您對音樂有熱情，歡迎參加我們的試音，我們歡迎各種樂器的樂手加入！",
     href: "/audition",
     icon: UserGroupIcon,
   },
@@ -43,7 +43,8 @@ export const menusItems = [
     id: 4,
     name: "聯絡我們",
     description: "與我們的樂團經理聯絡",
-    fullDescription: "如果你有任何問題或者合作的機會，歡迎與我們的樂團經理聯絡，我們將會盡快回覆你。",
+    fullDescription: "\n" +
+      "如果您對樂團有任何問題或意見，歡迎與我們聯絡，我們會盡快回覆您。",
     href: "/contact",
     icon: ChatBubbleLeftEllipsisIcon,
   }
