78e14ec005b52e6d83a4ccfb4e4ef910934e6417
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-10-04 00:44:03 +0800

    Add brightness to Carpe logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,9 @@ export default function Footer () {
   return (
     <>
       <div className="flex lg:flex-row flex-col lg:space-y-0 space-y-10 text-slate-50 bg-gradient-to-r from-slate-900 to-slate-950 lg:px-16 px-6 py-12 lg:space-x-8 items-center">
-        <a href="/"><Image src={logo} alt={"Carpe Symphonic Band"} className="h-24 w-fit"/></a>
+        <a href="/">
+          <Image src={logo} alt={"Carpe Symphonic Band"} className="h-24 w-fit brightness-150"/>
+        </a>
         <div className="flex flex-col">
           <div className="flex lg:space-x-8 lg:space-y-0 space-y-4 flex-col lg:flex-row">
             {filteredItems.map((menu) => (
